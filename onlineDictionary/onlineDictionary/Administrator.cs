ede3dc4c28b7e43492662bdb6b531139f70d54ce
Constantin Florin-George <constantingeorge150@gmail.com>
2024-03-06 18:47:27 +0200

    added users

--- onlineDictionary/onlineDictionary/Administrator.cs
+++ onlineDictionary/onlineDictionary/Administrator.cs
@@ -1,25 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json;
+
 
 namespace onlineDictionary
 {
     internal class Administrator
     {
         private bool _isLogged { get; set; }
-        private List<admin> _users;
-        struct admin
+        private List<admin> _users = new List<admin>();
+        public struct admin
         {
-            private string _username;
-            private string _password;
+            public string Username {  get; set; }
+            public string Password { get; set; }
         }
-           
 
-        private void ReadCredentials()
+        public Administrator() { }
+
+        public void ReadCredentials()
         {
+            try
+            {
+                string jsonText = File.ReadAllText("admins.json");
+                _users = JsonSerializer.Deserialize<List<admin>>(jsonText);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Fisier negasit");
+            }
+            catch (JsonException ex)
+            {
+                Console.WriteLine("Eroare la parsarea JSON-ului: " + ex.Message);
+            }
+        }
 
+        public bool SearchUser(String username, String password)
+        {
+            foreach(var user in _users)
+            {
+                if (user.Username == username && user.Password == password)
+                    return true;
+            }
+            return false;
         }
 
     }
--- onlineDictionary/onlineDictionary/AdministratorWindow.xaml.cs
+++ onlineDictionary/onlineDictionary/AdministratorWindow.xaml.cs
@@ -20,9 +20,42 @@ namespace onlineDictionary
     /// </summary>
     public partial class AdministratorWindow : Window
     {
+        Administrator admin = new Administrator();
+
         public AdministratorWindow()
         {
             InitializeComponent();
+            admin.ReadCredentials();
+            usernameText.Visibility = Visibility.Visible;
+            username.Visibility = Visibility.Visible;
+            passwordText.Visibility = Visibility.Visible;
+            password.Visibility = Visibility.Visible;
+            incorectCredentials.Visibility = Visibility.Collapsed;
+        }
+
+        private void username_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter && username.IsFocused)
+            {
+                password.Focus();
+            }
+        }
+
+        private void password_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter && password.IsFocused)
+            {
+                String userText = username.Text;
+                String passText = password.Text;
+                if (admin.SearchUser(userText, passText))
+                {
+                    MessageBox.Show("Utilizatorul exista");
+                }
+                else
+                {
+                    incorectCredentials.Visibility = Visibility.Visible;
+                }
+            }
         }
     }
 }
