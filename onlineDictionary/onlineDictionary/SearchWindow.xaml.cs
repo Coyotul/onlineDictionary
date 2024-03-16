a56be6f67b6330b0ab35d98cc4717aa537da117b
Constantin Florin-George <constantingeorge150@gmail.com>
2024-03-16 12:33:46 +0200

    finished searchWindow

--- onlineDictionary/onlineDictionary/SearchWindow.xaml.cs
+++ onlineDictionary/onlineDictionary/SearchWindow.xaml.cs
@@ -19,9 +19,48 @@ namespace onlineDictionary
     /// </summary>
     public partial class SearchWindow : Window
     {
+        String category = null;
+        Words _words = new Words();
         public SearchWindow()
         {
             InitializeComponent();
+            _words.GetWords();
+        }
+
+        private void search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            wordListBox.Items.Clear();
+            _words.GetWords();
+            foreach (var word in _words.ReturnWords())
+            {
+                ListBoxItem newItem = new ListBoxItem();
+                newItem.Content = word.Word;
+                newItem.MouseDoubleClick += WordItem_MouseDoubleClick;
+                SolidColorBrush whiteBrush = new SolidColorBrush(Colors.White);
+                newItem.Background = whiteBrush;
+                if (word.Word.StartsWith(search.Text))
+                {
+                    if (category == null)
+                        wordListBox.Items.Add(newItem);
+                    else if (category == word.Category)
+                        wordListBox.Items.Add(newItem);
+                }
+            }
+        }
+
+        private void WordItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var listBoxItem = (ListBoxItem)sender;
+            string selectedWord = (string)listBoxItem.Content;
+            string description = _words.GetWord(selectedWord).Description;
+            string category = _words.GetWord(selectedWord).Category;
+            string imageSource = _words.GetWord(selectedWord).ImageSource;
+            new WordPage(selectedWord, description, imageSource, category, false).Show();
+        }
+
+        private void category_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            category = categoryBox.Text;
         }
     }
 }
